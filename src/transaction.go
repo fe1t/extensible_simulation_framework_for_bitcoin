7d0258f0ee2aad27f1d4fc965323cb24d051982b
Nuttapon Thanitsukkan <nuttapon.t@ku.th>
2018-04-24 13:22:59 +0700

    manage new transaction.go

--- src/transaction.go
+++ src/transaction.go
@@ -22,6 +22,22 @@ type Transaction struct {
 	Vout []TXOutput
 }
 
+type TXInput struct {
+	Txid      []byte
+	Vout      int
+	Signature []byte
+	PublicKey []byte
+}
+
+type TXOutput struct {
+	Value         int
+	PublicKeyHash []byte
+}
+
+type TXOutputs struct {
+	Outputs []TXOutput
+}
+
 func (tx Transaction) SetID() {
 	var (
 		hash   [32]byte
@@ -262,4 +278,47 @@ func DeserializeTransaction(data []byte) Transaction {
 	}
 
 	return tx
+}
+
+func (inTx *TXInput) UsesKey(pubKeyHash []byte) bool {
+	return bytes.Compare(pubKeyHash, HashPubKey(inTx.PublicKey)) == 0
+}
+
+func (outTx *TXOutput) Lock(address []byte) {
+	pubKeyHash := Base58Decode(address)
+	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-checksumLength]
+	outTx.PublicKeyHash = pubKeyHash
+}
+
+func (outTx *TXOutput) IsLockedWithKey(pubKeyHash []byte) bool {
+	return bytes.Compare(outTx.PublicKeyHash, pubKeyHash) == 0
+}
+
+func NewTXOutput(value int, address string) *TXOutput {
+	txo := &TXOutput{value, nil}
+	txo.Lock([]byte(address))
+
+	return txo
+}
+
+func SerializeOutputs(data TXOutputs) []byte {
+	var buff bytes.Buffer
+
+	gobEncoder := gob.NewEncoder(&buff)
+	err := gobEncoder.Encode(data)
+	if err != nil {
+		log.Panic(err)
+	}
+	return buff.Bytes()
+}
+
+func DeserializeOutputs(data []byte) TXOutputs {
+	var outputs TXOutputs
+
+	gobDecoder := gob.NewDecoder(bytes.NewReader(data))
+	err := gobDecoder.Decode(&outputs)
+	if err != nil {
+		log.Panic(err)
+	}
+	return outputs
 }
--- src/transaction_output.go
+++ src/transaction_output.go
@@ -1,55 +1,55 @@
 package main
 
-import (
-	"bytes"
-	"encoding/gob"
-	"log"
-)
+// import (
+// 	"bytes"
+// 	"encoding/gob"
+// 	"log"
+// )
 
-type TXOutput struct {
-	Value         int
-	PublicKeyHash []byte
-}
+// type TXOutput struct {
+// 	Value         int
+// 	PublicKeyHash []byte
+// }
 
-type TXOutputs struct {
-	Outputs []TXOutput
-}
+// type TXOutputs struct {
+// 	Outputs []TXOutput
+// }
 
-func (outTx *TXOutput) Lock(address []byte) {
-	pubKeyHash := Base58Decode(address)
-	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-checksumLength]
-	outTx.PublicKeyHash = pubKeyHash
-}
+// func (outTx *TXOutput) Lock(address []byte) {
+// 	pubKeyHash := Base58Decode(address)
+// 	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-checksumLength]
+// 	outTx.PublicKeyHash = pubKeyHash
+// }
 
-func (outTx *TXOutput) IsLockedWithKey(pubKeyHash []byte) bool {
-	return bytes.Compare(outTx.PublicKeyHash, pubKeyHash) == 0
-}
+// func (outTx *TXOutput) IsLockedWithKey(pubKeyHash []byte) bool {
+// 	return bytes.Compare(outTx.PublicKeyHash, pubKeyHash) == 0
+// }
 
-func NewTXOutput(value int, address string) *TXOutput {
-	txo := &TXOutput{value, nil}
-	txo.Lock([]byte(address))
+// func NewTXOutput(value int, address string) *TXOutput {
+// 	txo := &TXOutput{value, nil}
+// 	txo.Lock([]byte(address))
 
-	return txo
-}
+// 	return txo
+// }
 
-func SerializeOutputs(data TXOutputs) []byte {
-	var buff bytes.Buffer
+// func SerializeOutputs(data TXOutputs) []byte {
+// 	var buff bytes.Buffer
 
-	gobEncoder := gob.NewEncoder(&buff)
-	err := gobEncoder.Encode(data)
-	if err != nil {
-		log.Panic(err)
-	}
-	return buff.Bytes()
-}
+// 	gobEncoder := gob.NewEncoder(&buff)
+// 	err := gobEncoder.Encode(data)
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+// 	return buff.Bytes()
+// }
 
-func DeserializeOutputs(data []byte) TXOutputs {
-	var outputs TXOutputs
+// func DeserializeOutputs(data []byte) TXOutputs {
+// 	var outputs TXOutputs
 
-	gobDecoder := gob.NewDecoder(bytes.NewReader(data))
-	err := gobDecoder.Decode(&outputs)
-	if err != nil {
-		log.Panic(err)
-	}
-	return outputs
-}
+// 	gobDecoder := gob.NewDecoder(bytes.NewReader(data))
+// 	err := gobDecoder.Decode(&outputs)
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+// 	return outputs
+// }
