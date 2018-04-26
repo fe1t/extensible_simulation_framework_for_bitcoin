1dc7ad65b8f211b6768a6ebb18dbe75408e30f3c
Nuttapon Thanitsukkan <nuttapon.t@ku.th>
2018-04-27 02:20:17 +0700

    serf as memberlist wrapper, TODO: handling Query

--- src/server.go
+++ src/server.go
@@ -8,11 +8,11 @@ import (
 	"io"
 	"log"
 	"net"
-	"strconv"
 	"sync"
-	"time"
 
 	"github.com/clockworksoul/smudge"
+	"github.com/hashicorp/memberlist"
+	"github.com/hashicorp/serf/serf"
 )
 
 type addr struct {
@@ -73,6 +73,7 @@ const (
 	nodeVersion   = 1
 	commandLength = 12
 	baseAddress   = "127.0.0.1"
+	UDPBufferSize = 2000
 )
 
 var (
@@ -81,61 +82,102 @@ var (
 	knownNodes      = []string{fmt.Sprintf("%s:3000", baseAddress)}
 	blocksInTransit = [][]byte{}
 	mempool         = make(map[string]Transaction)
+	broadcasts      *memberlist.TransmitLimitedQueue
+	thisNode        *serf.Serf
 )
 
-func (m MyStatusListener) OnChange(node *smudge.Node, status smudge.NodeStatus) {
-	fmt.Printf("Node %s is now status %s\n", node.Address(), status)
+type broadcast struct {
+	msg    []byte
+	notify chan<- struct{}
 }
 
-func (m MyBroadcastListener) OnBroadcast(b *smudge.Broadcast) {
-	// broadcastMsg from b.Origin().Address()
-	fmt.Println("OnBroadcast triggered")
-	go handleConnection(b.Bytes(), Bc)
+type delegate struct {
+	serf *serf.Serf
 }
 
-// ConfigServer configuration for Smudge Library
-func ConfigServer() error {
-	port, err := strconv.Atoi(NODE_ID)
+type update struct {
+	Action string // add, del
+	Data   map[string]string
+}
+
+func (b *broadcast) Invalidates(other memberlist.Broadcast) bool {
+	return false
+}
+
+func (b *broadcast) Message() []byte {
+	return b.msg
+}
+
+func (b *broadcast) Finished() {
+	if b.notify != nil {
+		close(b.notify)
+	}
+}
+
+func (d *delegate) NodeMeta(limit int) []byte {
+	return []byte{}
+}
+
+func (d *delegate) NotifyMsg(buf []byte) {
+	if len(buf) == 0 {
+		return
+	}
+	fmt.Println("Received message")
+	go handleConnection(buf, Bc)
+	// go handleConnection(b)
+}
+
+func (d *delegate) GetBroadcasts(overhead, limit int) [][]byte {
+	return broadcasts.GetBroadcasts(overhead, limit)
+}
+
+func (d *delegate) LocalState(join bool) []byte {
+	fmt.Println("push pulllll")
+	return []byte{}
+}
+
+func (d *delegate) MergeRemoteState(buf []byte, isJoin bool) {}
+
+func configServer() error {
+	memberlistConfig := memberlist.DefaultLocalConfig()
+	memberlistConfig.Name = nodeAddress
+	memberlistConfig.BindAddr = baseAddress
+	if _, err := fmt.Sscanf(NODE_ID, "%d", &memberlistConfig.BindPort); err != nil {
+		return err
+	}
+	memberlistConfig.PushPullInterval = 0
+	memberlistConfig.UDPBufferSize = UDPBufferSize
+	memberlistConfig.Name = nodeAddress
+
+	eventCh := make(chan serf.Event, 16)
+	serfConfig := serf.DefaultConfig()
+	serfConfig.NodeName = nodeAddress
+	serfConfig.EventCh = eventCh
+	serfConfig.RejoinAfterLeave = true
+	serfConfig.QuerySizeLimit = 1500
+	// serfConfig.EventCh = serfEvents
+	serfConfig.MemberlistConfig = memberlistConfig
+
+	m, err := serf.Create(serfConfig)
+	thisNode = m
+
 	if err != nil {
 		return err
 	}
-
-	// Set configuration options
-	smudge.SetListenIP(net.ParseIP(baseAddress))
-	smudge.SetListenPort(port)
-	smudge.SetHeartbeatMillis(500)
-	smudge.SetMaxBroadcastBytes(2000)
-	smudge.SetLogThreshold(smudge.LogOff)
-	// smudge.SetMulticastEnabled(false)
-	smudge.SetClusterName("KU")
-
-	smudge.AddStatusListener(MyStatusListener{})
-	smudge.AddBroadcastListener(MyBroadcastListener{})
-
-	// Add a new remote node. Currently, to join an existing cluster you must
-	// add at least one of its healthy member nodes.
-
 	if nodeAddress != knownNodes[0] {
-		node, err := smudge.CreateNodeByAddress(knownNodes[0])
-		if err == nil {
-			smudge.AddNode(node)
-		} else {
+		_, err := m.Join(knownNodes, false)
+		if err != nil {
 			return err
 		}
 	}
-
-	// start the server
-	go func() {
-		smudge.Begin()
-	}()
-
-	time.Sleep(time.Second * 5)
-
-	// Handle SIGINT and SIGTERM.
-	// quit := make(chan os.Signal, 2)
-	// signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	// <-quit
-
+	broadcasts = &memberlist.TransmitLimitedQueue{
+		NumNodes: func() int {
+			return m.NumNodes()
+		},
+		RetransmitMult: 10,
+	}
+	node := m.LocalMember()
+	fmt.Printf("Local member %s:%d\n", node.Addr, node.Port)
 	return nil
 }
 
@@ -148,14 +190,13 @@ func StartServer(nodeID, minerAddress string) {
 
 	wg.Add(1)
 	go func() {
-		err = ConfigServer()
+		err = configServer()
+		if err != nil {
+			log.Panic(err)
+		}
 		wg.Done()
 	}()
 	wg.Wait()
-
-	if err != nil {
-		log.Panic(err)
-	}
 
 	if Bc == nil {
 		Bc = NewBlockchain(nodeID)
@@ -476,8 +517,10 @@ func handleVersion(request []byte, bc *Blockchain) {
 func sendBlock(addr string, b *Block) {
 	payload := gobEncode(block{nodeAddress, addr, Serialize(b)})
 	request := append(commandToBytes("block"), payload...)
-	smudge.BroadcastBytes(request)
-	// sendData(addr, request)
+	broadcasts.QueueBroadcast(&broadcast{
+		msg:    request,
+		notify: nil,
+	})
 }
 
 func sendData(address string, request []byte) {
@@ -507,29 +550,37 @@ func sendData(address string, request []byte) {
 func sendGetBlocks(addr string) {
 	encodedGetBlocks := gobEncode(getblocks{nodeAddress, addr})
 	request := append(commandToBytes("getblocks"), encodedGetBlocks...)
-	smudge.BroadcastBytes(request)
-	// sendData(addr, request)
+	broadcasts.QueueBroadcast(&broadcast{
+		msg:    request,
+		notify: nil,
+	})
 }
 
 func sendGetData(addr, kind string, id []byte) {
 	encodedGetData := gobEncode(getdata{nodeAddress, addr, kind, id})
 	request := append(commandToBytes("getdata"), encodedGetData...)
-	smudge.BroadcastBytes(request)
-	// sendData(addr, request)
+	broadcasts.QueueBroadcast(&broadcast{
+		msg:    request,
+		notify: nil,
+	})
 }
 
 func sendTx(addr string, tnx *Transaction) {
 	encodedTx := gobEncode(tx{nodeAddress, addr, SerializeTransaction(*tnx)})
 	request := append(commandToBytes("tx"), encodedTx...)
-	smudge.BroadcastBytes(request)
-	// sendData(addr, request)
+	broadcasts.QueueBroadcast(&broadcast{
+		msg:    request,
+		notify: nil,
+	})
 }
 
 func sendInventory(addr, kind string, blockHashes [][]byte) {
 	encodedInventory := gobEncode(inventory{nodeAddress, addr, kind, blockHashes})
 	request := append(commandToBytes("inv"), encodedInventory...)
-	smudge.BroadcastBytes(request)
-	// sendData(addr, request)
+	broadcasts.QueueBroadcast(&broadcast{
+		msg:    request,
+		notify: nil,
+	})
 }
 
 func sendVersion(addr string, bc *Blockchain) {
@@ -537,8 +588,10 @@ func sendVersion(addr string, bc *Blockchain) {
 	lastHeight := bc.GetLastBlockHeight()
 	encodedLastHeight := gobEncode(version{nodeAddress, addr, nodeVersion, lastHeight})
 	request := append(commandToBytes("version"), encodedLastHeight...)
-	smudge.BroadcastBytes(request)
-	// sendData(addr, request)
+	_, err := thisNode.Query("version", request, nil)
+	if err != nil {
+		log.Panic(err)
+	}
 }
 
 func nodeIsKnown(address string) bool {
